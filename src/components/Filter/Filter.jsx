e8f944c76a32590fc88a001a71b7cb8d7c064bb1
AlexSakhno <front-end-san@yandex.ru>
2022-06-14 07:40:56 +0300

    refactoring

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import FilterItem from '../FilterItem/FilterItem'
 import classes from './Filter.module.scss'
+import { arrFilters } from '../../models/models'
 
 export default function Filter() {
+	const filters = arrFilters.map(filter => {
+		return <FilterItem key={filter} name={filter} />
+	})
 	return (
 		<div className={classes.filterContainer}>
 			<span className={classes.filterTitle}>Количество пересадок</span>
-			<ul className={classes.filterList}>
-				<FilterItem name='ALL' />
-				<FilterItem name='0_TRANS' />
-				<FilterItem name='1_TRANS' />
-				<FilterItem name='2_TRANS' />
-				<FilterItem name='3_TRANS' />
-			</ul>
+			<ul className={classes.filterList}>{filters}</ul>
 		</div>
 	)
 }
---
+++ src/models/models.js
@@ -0,0 +1,2 @@
+// Filters transition
+export let arrFilters = ['ALL', '0_TRANS', '1_TRANS', '2_TRANS', '3_TRANS']
